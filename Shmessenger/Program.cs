a535b3417cbd2bf17b57225b2dec0f2a0c392c1f
lvivskixlop <44100296+lvivskixlop@users.noreply.github.com>
2020-05-27 17:15:31 +0300

    продовження

--- Shmessenger/Program.cs
+++ Shmessenger/Program.cs
@@ -12,20 +12,51 @@ namespace Shmessenger
     {
         static void Main(string[] args)
         {
-                        
+            Thread recieveThread = new Thread(recieveMessage);
+            recieveThread.Start();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Console.WriteLine("You pressed any key. It seems like you want something. What is it?\nSend Message(y)/Continiue waiting(n) ");
+                    string answer = Console.ReadLine();
+                    if (answer == "y")
+                    {
+                        sendMessage();
+                    }
+                    else if(answer == "n")
+                    {
+                        Console.WriteLine("ok");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOUR ANSWER IS WRONG! GTFO!");
+                        break;
+                    }
+                }
+            }
+            recieveThread.Abort();
+            
         }
 
         static void sendMessage()
         {
             TcpClient client = new TcpClient();
             Console.WriteLine("Input IP address you want to message to: ");
-            IPAddress recieverIP = IPAddress.Parse(Console.ReadLine());
+            string ipString = Console.ReadLine();
+            IPAddress recieverIP = IPAddress.Parse(ipString);            
+
             client.Connect(recieverIP, 80);
+
             byte[] send = new byte[2048];
+            Console.WriteLine("Type your message here: \n");
+            string messageString = Console.ReadLine();
+            send = Encoding.ASCII.GetBytes(messageString);
             NetworkStream stream = client.GetStream();
             stream.Write(send, 0, send.Length);
             stream.Close();
             client.Close();
+            Console.WriteLine("Message sent.");
         }//Надіслати повідомлення
 
         static void recieveMessage()
@@ -47,6 +78,8 @@ namespace Shmessenger
                 client.Close();
             }
         }//Отримання повідомлення
+
+        
 
     }
 }
